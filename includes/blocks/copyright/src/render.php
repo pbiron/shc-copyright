5eab28a7156f384ad325a7b6aea2eeb4cecc3999
Paul Biron <paul@sparrowhawkcomputing.com>
2023-09-02 10:44:32 -0600

    Block: use JSX for our block editor script.

---
+++ includes/blocks/copyright/src/render.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Render template for the shc-copyright/copyright block.
+ *
+ * @since 0.9.1
+ *
+ * The following global variables are in scope when this template is loaded:
+ *
+ * @var string[] $attributes    The block attributes.
+ * @var WP_Block $block         The parsed block
+ * @var string   $content       The block content (inner blocks).
+ * @var string   $template_path The path to this file.
+ *
+ * @package shc-copyright
+ */
+
+$launch_year  = $attributes['launchYear'];
+$current_year = (int) wp_date( 'Y' );
+
+if ( ! $launch_year || $launch_year === $current_year ) {
+	$copyright_years = $current_year;
+} else {
+	$copyright_years = "{$launch_year}&ndash;{$current_year}";
+}
+
+$content = str_replace( 'SHC_COPYRIGHT_YEARS_VALUE', $copyright_years, $content );
+
+echo wp_kses_post( $content );
